e479e3166c86d6dc1b18122ddf4db154e1326762
Prathamesh Devadiga <71978538+devadigapratham@users.noreply.github.com>
2023-05-27 22:12:06 +0530

    Update Banner.jsx

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -13,8 +13,8 @@ const Banner = () => {
                     Your platform for peer-to-peer book lending and selling. Connect with fellow readers, discover new titles, and share your favorite stories. Empowering book lovers to exchange knowledge and build a community through the love of literature. Join us in unlocking a world of literary connections and adventures
                     </p>
                     <div className="ctas">
-                        <div className="banner-cta">Read More</div>
-                        <div className="banner-cta v2">Shop Now</div>
+                        <div className="banner-cta">Sell / Rent Your Books</div>
+                        <div className="banner-cta v2">Shop / Rent Books</div>
                     </div>
                 </div>
             </div>
